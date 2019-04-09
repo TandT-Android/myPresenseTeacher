b641872314313c747f0e67f58da6c978006545c8
kumarpawan123 <pawansaw0123456789@gmail.com>
2019-04-10 04:14:43 +0530

    Auth and Recycler view for retrieve roll no.

---
+++ app/src/main/java/boys/indecent/mypresense/teacher/Adapter.java
@@ -0,0 +1,36 @@
+package boys.indecent.mypresense.teacher;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class Adapter  extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private ArrayList<String> Roll_No=new ArrayList<>();
+    private Context nContext;
+
+
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/boys/indecent/mypresense/teacher/StudentsLists.java
@@ -0,0 +1,33 @@
+package boys.indecent.mypresense.teacher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class StudentsLists extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_students_lists);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new Adapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+
+
+
+
+
+    }
+}
