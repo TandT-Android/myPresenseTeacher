48e827e2867bc1c4206b4d8fe050cc001be8a9b8
arvinkr19 <44765895+arvinkr19@users.noreply.github.com>
2019-04-16 07:05:32 +0530

    Add recyclerView for all roll numbers present in a class

--- app/src/main/java/boys/indecent/mypresense/teacher/MainActivity.java
+++ app/src/main/java/boys/indecent/mypresense/teacher/MainActivity.java
@@ -1,16 +1,40 @@
 package boys.indecent.mypresense.teacher;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this,SignInActivity.class);
+        Intent intent = new Intent(this, StudentsLists.class);
         startActivity(intent);
-    }
+
+
+         }
 }
